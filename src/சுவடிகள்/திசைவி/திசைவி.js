79b45908f6b412638230c6a1f912808d9c035aa3
log10 <lokesh.noble@gmail.com>
2019-05-11 19:33:23 +0530

    adding router

---
+++ src/சுவடிகள்/திசைவி/திசைவி.js
@@ -0,0 +1,28 @@
+import {சிறுகதை_திசைவி} from "./சிறுகதை.js";
+
+const தளம் = document.querySelector(".தளம்");
+
+const நிறுவு = (பக்கம், பின்பழை = ()=>{}) => {
+    தளம்.innerHTML = பக்கம்;
+    பின்பழை();
+};
+
+const திசைவி = முகவரி => {
+    let பகுதி = முகவரி.split('#');
+    if(பகுதி.length == 2) {
+        let பக்கம் = decodeURI(பகுதி[1]);
+        if(பக்கம்.match('^சிறுகதைகள்') != null) {
+            சிறுகதை_திசைவி.திசைவி(பக்கம், நிறுவு)
+        }
+    } else {
+        import('../../பாகங்கள்/விழித்தளம்/விழித்தளம்.html').then(நிறுவு);
+    }
+};
+
+window.addEventListener('hashchange', வினை => {
+    திசைவி(வினை.newURL);
+});
+
+window.addEventListener('load', வினை => {
+    திசைவி(window.location.href);
+});
--- src/முகப்பு.js
+++ src/முகப்பு.js
@@ -1,13 +1,21 @@
 import "./பாங்குகள்.css";
 import "./பாகங்கள்/விழித்தளம்/விழித்தளம்.css";
+import "./பாங்குகள்/சிறுகதைகள்.css"
 
-window.addEventListener('load', (event) => {
-    import('./பாகங்கள்/விழித்தளம்/விழித்தளம்.html').then(function (page) {
-        document.querySelector(".தளம்").innerHTML = page;
-    });
-    document.querySelector(".தலையங்கம்").addEventListener('click',evt => {
-        import('./பாகங்கள்/தொடு/தொடு.html').then(function (page) {
-             document.querySelector(".தளம்").innerHTML = page;
-         });
+import "./சுவடிகள்/திசைவி/திசைவி.js";
+
+window.addEventListener('load', வினை => {
+//    document.querySelector(".தலையங்கம்").addEventListener('click',evt => {
+//        import('./பாகங்கள்/தொடு/தொடு.html').then(function (page) {
+//             document.querySelector(".தளம்").innerHTML = page;
+//         });
+//    });
+    document.querySelector(".சிறுகதையின்கொக்கி").addEventListener('click',வினை => {
+        let முகவரி = '#சிறுகதைகள்';
+        if(window.location.href.includes('#')) {
+            window.location.href = window.location.href.split('#')[0] + முகவரி;
+        } else {
+            window.location += முகவரி;
+        }
     });
 });
