50d434cdcd1eb85f5737e9a84d2f2741a247331c
log10 <lokesh.noble@gmail.com>
2020-04-12 20:48:36 +0530

    இணைக்கும் ஏந்துகள் சேர்க்கப்பட்டது

--- utils/Main.java
+++ utils/Main.java
@@ -1,4 +1,6 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -10,15 +12,22 @@ public class Main {
                         Paths.get("C:\\Users\\loghithavani\\Desktop\\Development\\kadhaigal\\src\\களஞ்சியம்\\சிறுகதைகள்\\தேர்வு1.html"
                         )));
         String[] xs = x.replaceAll("\\\\t", "&emsp;&emsp;")
-                .replaceAll("(\\\\r)?\\\\n", " <br> ").split(" ");
+                .replaceAll("(\\\\r)?\\\\n", " <br> ")
+                .replaceAll("<s (.*?)>", " <span இணைப்பு=\"$1\" class=\"இணைப்பு\"> ")
+                .replaceAll("<se (.*?)>", " <span வெளி_இணைப்பு=\"$1\" class=\"இணைப்பு\"> ")
+                .split(" ");
         StringBuilder sb = new StringBuilder();
         for(String s: xs) {
             sb.append(s.replaceAll("<h>", "<div style=\"text-align: center; font-weight: bold\">")
                     .replaceAll("</h>", "</div>")
                     .replaceAll("<c>", "<div><div style=\"padding-left: 15%\">")
                     .replaceAll("</c>", "</div></div>")
+                    .replaceAll("</s>", "</span>")
             ).append(" ");
         }
-	    System.out.println(sb);
+        //System.out.println(sb);
+        PrintWriter pw = new PrintWriter(new File("C:\\Users\\loghithavani\\Desktop\\Development\\kadhaigal\\src\\களஞ்சியம்\\சிறுகதைகள்\\தேர்வு2.html"));
+        pw.println(sb);
+        pw.close();
     }
 }
